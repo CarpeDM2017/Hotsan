ebbaa276fe66e1e27348f68aa413ecaac3cde64a
Jiyong <qhrdmawhro@naver.com>
2017-09-05 21:23:13 +0900

    Transfer Files from Previous Repository

---
+++ main.py
@@ -0,0 +1,10 @@
+import coinone_tools
+import tool_collections
+
+payload_dict_raw = {"currency": ["btc", "bch", "eth", "xrp", "etc", "qtum"], "period": ["day"]}
+payloads = tool_collections.payload_parse(payload_dict_raw)
+
+for parsed_payload in payloads:
+    Hotsan = coinone_tools.HotSanClient(url="trades/", payload=parsed_payload)
+    response = Hotsan.get_response(is_public=True)
+    tool_collections.json_to_file(response, filename=parsed_payload["currency"], filepath_dir= "\Users\User\Desktop\Coinprice DB")

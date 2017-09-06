9c5d727fca1423d74b34746ce5d14a942c5a5ffc
Jiyong <qhrdmawhro@naver.com>
2017-09-06 17:59:44 +0900

    Modified daily worker batch files

--- database/db_coinone.py
+++ database/db_coinone.py
@@ -1,16 +1,24 @@
 from exchange import coinone_api as coinone
 from tools import formatters
 from tools import parsers
+import sys
 
 """Script for downloading Coin price DB from coinone server
 should run daily on specified time."""
 
 # Specify Which transaction records to fetch as dict format.
-payload_dict_raw = {"currency": ["btc", "bch", "eth", "xrp", "etc", "qtum"], "period": ["day"]}
-payloads = parsers.payload_parse(payload_dict_raw)
+payload_dict_daily = {"currency": ["btc", "bch", "eth", "xrp", "etc", "qtum"], "period": ["day"]}
+payload_dict_hourly = {"currency": ["btc", "bch", "eth", "xrp", "etc", "qtum"], "period": ["hour"]}
+
+# Check if it's daily request or hourly request
+if sys.argv[1] == "hourly":
+    payloads = parsers.payload_parse(payload_dict_hourly)
+else :
+    payloads = parsers.payload_parse(payload_dict_daily)
 
 # Will fetch specified records from server.
 for parsed_payload in payloads:
     Hotsan = coinone.HotSanClient(url="trades/", payload=parsed_payload)
     response = Hotsan.get_response(is_public=True)
-    formatters.json_to_file(response, filename=parsed_payload["currency"], filepath_dir= "\Users\User\Desktop\Coinprice DB")
+    formatters.json_to_file(response, filename=parsed_payload["currency"],
+                            filepath_dir= "\Users\User\Desktop\Coinprice DB")
--- database/workers/hour_db.bat
+++ database/workers/hour_db.bat
@@ -1,3 +1,8 @@
+:: This will download coinone transaction records for previous 24 hours
+:: For now, windows run this batch file at scheduled time (01AM, 02AM)
+@echo off
+:: Wait for 15 seconds before starting.
 timeout 15
-python C:\Users\User\Desktop\sunwooang\Hotsan\hourly_main.py
-exit
+:: Assign parameters, Database to fetch.
+SET interval=hourly
+python C:\Users\User\Desktop\sunwooang\Hotsan\database\db_coinone.py %interval%
